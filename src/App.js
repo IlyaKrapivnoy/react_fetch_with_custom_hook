831969434bb9d93aa9a3cbb35a11288ee9a4b4de
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-24 18:11:48 +0300

    display data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
+import useFetch from './useFetch';
 
 function App() {
-    return <div className='App'></div>;
+    const { data, loading, error } = useFetch(
+        'https://v2.jokeapi.dev/joke/Any'
+    );
+
+    if (loading) {
+        return <h2>LOADING...</h2>;
+    }
+    if (error) {
+        console.log(error);
+    }
+
+    return (
+        <div className='App'>
+            <h1>
+                {data?.setup} : {data?.delivery}
+            </h1>
+        </div>
+    );
 }
 
 export default App;
